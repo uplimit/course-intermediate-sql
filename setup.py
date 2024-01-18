4c8a1164d77fc9fa1959548061d2fbcced20d44c
Sourabh Bajaj <1517779+sb2nov@users.noreply.github.com>
2024-01-17 23:00:56 -0800

    Update setup.py (#25)

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(
     name='sql_course',
     version='1.0.0',
-    url='https://github.com/sb2nov/sql-cc',
+    url='https://github.com/uplimit/course-intermediate-sql',
     author='Sourabh Bajaj, Reinier Koops',
-    description='Run an SQL query for the "Intermediate SQL Course" on CoRise',
+    description='Run an SQL query for the "Intermediate SQL Course" on Uplimit',
     long_description=open('README.md').read(),
     license='GPL3',
     packages=['sql_course'],
